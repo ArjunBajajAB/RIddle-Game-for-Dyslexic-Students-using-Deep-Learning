b665235420c215a9e9be79c368bb6f788d9263a7
Arjun Bajaj <arjunbajaj2000@gmail.com>
2020-10-02 18:55:50 +0530

    Add separate frames for portion on window

--- Riddle_Game.py
+++ Riddle_Game.py
@@ -11,31 +11,58 @@ class Riddle_Game(object):
         self.root.title("Riddle Game") #Set the title of the window
         self.root.resizable(0,0)
         self.button_font = tkFont.Font(family="Arial",size=20,weight="bold")
+        self.heading_font = tkFont.Font(family="Courier", size=50, weight="bold")
         self.starter()
         self.root.mainloop()
 
     def starter(self):
-        self.background_header()
-        self.play_comp_button = Button(self.root, text="Play with computer",activebackground="Yellow",bd=3,bg="black",fg="white",
-                                       command=self.play_comp,font=self.button_font, justify=CENTER, height=2, width=20)
-        self.play_comp_button.place(x=300,y=500)
-        self.play_friend_button = Button(self.root, text="Play with a friend", activebackground="Yellow", bd=3,
-                                       bg="black", fg="white",command=self.play_friend, font=self.button_font, justify=CENTER,height=2, width=20)
-        self.play_friend_button.place(x=660, y=500)
+        self.first_frame()
 
 
-    def background_header(self):
-        self.header_image = ImageTk.PhotoImage(file="images/Header.png")
-        self.header_label = Label(self.root, image=self.header_image)
-        self.header_label.place(x=0, y=0)
+    def first_frame(self):
+        self.background()
+        self.frame1 = Frame(self.root, height=300, width= 1000,bg="White")
+        self.frame1.pack(side= TOP)
+        self.frame2 =  Frame(self.root, height=100, width=400,bg="White",padx=10,pady=10)
+        self.frame2.pack(side=LEFT)
+        self.frame3 = Frame(self.root, height=100, width=400, bg="White",padx=10,pady=10)
+        self.frame3.pack(side=RIGHT)
+        self.game_heading(self.frame1)
+        self.buttons(self.frame2,name="play_with_comp")
+        self.buttons(self.frame3,name="play_with_friend")
 
-    def background_solo(self):
+
+
+    def game_heading(self,frame):
+        self.heading1 = Label(frame,font=self.heading_font,fg="Black",justify=CENTER,text="THE",height=2,width=7)
+        self.heading1.pack(side=TOP)
+        self.heading2 = Label(frame, font=self.heading_font, fg="Black", justify=CENTER, text="RIDDLE", height=2, width=7)
+        self.heading2.pack(side=TOP)
+        self.heading3 = Label(frame, font=self.heading_font, fg="Black", justify=CENTER, text="GAME", height=2, width=7)
+        self.heading3.pack(side=TOP)
+
+
+    def background(self):
         self.background_image = ImageTk.PhotoImage(file="images/riddle.jpg")
         self.background_label = Label(self.root, image=self.background_image)
         self.background_label.place(x=0, y=0, relwidth=1, relheight=1)
 
+    def buttons(self,frame,name="None"):
+        if name=="play_with_comp":
+            self.play_comp_button = Button(self.frame2, text="Play with computer", activebackground="Yellow", bd=3,
+                                           bg="black", fg="white",
+                                           command=self.play_comp, font=self.button_font, justify=CENTER, height=2,
+                                           width=20)
+            self.play_comp_button.pack(side=LEFT)
+        if name=="play_with_friend":
+            self.play_friend_button = Button(self.frame3, text="Play with a friend", activebackground="Yellow", bd=3,
+                                             bg="black", fg="white", command=self.play_friend, font=self.button_font,
+                                             justify=CENTER, height=2, width=20)
+            self.play_friend_button.pack(side=RIGHT)
+
+
     def play_comp(self):
-        pass
+        self.root.delete("all")
 
     def play_friend(self):
         pass
