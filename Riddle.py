7d2c3445317c2ddc43ee6df819db118fa188f6fd
Arjun Bajaj <arjunbajaj2000@gmail.com>
2020-10-29 12:00:03 +0530

    Create working page 1 and 2

--- Riddle.py
+++ Riddle.py
@@ -10,98 +10,54 @@ class Riddle_Game(object):
         self.root.geometry("1200x800") #Define the size of the window or app
         self.root.title("Riddle Game") #Set the title of the window
         self.root.resizable(0,0)
-        self.background_solo(self.root)
-        self.creat_MainFrame()
-        self.heading_font_topic = tkFont.Font(family="Courier", size=40, weight="bold")
-        self.heading_font = tkFont.Font(family="Courier", size=50, weight="bold")
-        self.button_font = tkFont.Font(family="Arial",size=20,weight="bold")
-        self.first_frame()
+        self.text_font = tkFont.Font(family="Courier New", size=15, weight="bold")
+        self.heading_font = tkFont.Font(family="Verdana", size=40, weight="bold")
+        self.button_font = tkFont.Font(family="Playbill",size=15,weight="bold")
+        self.BackgroundImage(self.root)
+        self.first_page()
         self.root.mainloop()
 
-    def creat_MainFrame(self):
-        self.main_frame = Frame(self.root, height=750, width=1050)
-        self.main_frame.place(x=100, y=25)
-        self.background_frames(self.main_frame)
-
-    def background_solo(self,root):
-        self.background_image = ImageTk.PhotoImage(file="images/riddle_main.jpg")
-        self.background_label = Label(root, image=self.background_image)
-        self.background_label.grid(row=0, column=0)
-
-    def background_frames(self,root):
-        self.background_label_frame = Label(root, image=self.background_image)
-        self.background_label_frame.place(x=0, y=0)
-
-    def first_frame(self):
-        self.heading_frame= Frame(self.main_frame, height= 300, width=1000,bg="DodgerBlue4",bd=4)
-        self.heading_frame.pack(expand=True,fill=X)
-        self.background_frames(self.heading_frame)
-        self.game_heading()
-        self.content_frame = Frame(self.main_frame,height=300,width=1000,bg="DodgerBlue4",bd=4)
-        self.content_frame.pack(side=BOTTOM,expand=True,fill=BOTH,pady=15)
-        self.background_frames(self.content_frame)
-        self.first_frame_buttons()
-
-    def game_heading(self):
-        self.heading_line_1 = Label(self.heading_frame,font=self.heading_font, fg="Black",bg ="White", justify=CENTER, text="THE",height=2, width=7)
-        self.heading_line_1.place(x=400,y=0)
-        self.heading_line_2 = Label(self.heading_frame, font=self.heading_font, fg="Black",bg ="White", justify=CENTER, text="RIDDLE", height=2, width=7)
-        self.heading_line_2.place(x=400,y=100)
-        self.heading_line_3 = Label(self.heading_frame, font=self.heading_font, fg="Black",bg ="White", justify=CENTER, text="GAME", height=2, width=7)
-        self.heading_line_3.place(x=400,y=200)
-
-    def first_frame_buttons(self):
-        self.play_comp_button = Button(self.content_frame, text="Play with computer", activebackground="Yellow", bd=3,bg="black", fg="white",
-                                       command=self.play_comp, font=self.button_font, justify=CENTER, height=2,width=20)
-        self.play_comp_button.place(x=80,y=80)
-        self.play_friend_button = Button(self.content_frame, text="Play with a friend", activebackground="Yellow", bd=3,
-                                        bg="black", fg="white", command=self.play_friend, font=self.button_font,
-                                        justify=CENTER, height=2, width=20)
-        self.play_friend_button.place(x=550,y=80)
-        self.about_button = Button(self.content_frame, text="About", activebackground="Yellow", bd=3,
-                                        bg="black", fg="white", command=self.about, font=self.button_font,
-                                        justify=CENTER, height=2, width=20)
-        self.about_button.place(x=300,y=200)
+    def first_page(self):
+        self.CreateMainFrame("First")
+        self.content_frame = Frame(self.main_frame,height=100,width=800,bg="#1a0d00")
+        self.content_frame.place(x=200,y=450)
+        self.play_comp_button = Button(self.content_frame, text="Play with computer", activebackground="saddle brown", bd=3,bg="black", fg="white",
+                                       command=self.play_comp, font=self.button_font, justify=CENTER, height=2,width=15)
+        self.play_comp_button.place(x=20,y=10)
+        self.play_friend_button = Button(self.content_frame, text="Play with a friend", activebackground="saddle brown", bd=3,bg="black", fg="white",
+                                         command=self.play_friend, font=self.button_font,justify=CENTER, height=2, width=15)
+        self.play_friend_button.place(x=285,y=10)
+        self.about_button = Button(self.content_frame, text="About", activebackground="saddle brown", bd=3,bg="black", fg="white",
+                                   command=self.about, font=self.button_font,justify=CENTER, height=2, width=15)
+        self.about_button.place(x=550,y=10)
 
     def play_comp(self):
         self.main_frame.destroy()
-        self.creat_MainFrame()
-        self.heading_frame= Frame(self.main_frame,height=200,width=1100,bg="DodgerBlue4")
-        self.heading_frame.place(x=0,y=0)
-        self.heading_label = Label(self.heading_frame,font=self.heading_font_topic,fg="Black",height=2,width=35,text="Let us start with your details")
-        self.heading_label.pack(side=TOP)
-        self.content_frame = Frame(self.main_frame,height=400,width=1100,bg="DodgerBlue4")
-        self.content_frame.place(x=0,y=300)
-        self.background_frames(self.content_frame)
-        self.name_frame = Frame(self.content_frame, height=100, width=300)
-        self.name_frame.place(x=0, y=0)
-        self.name_label = Label(self.name_frame,bg="White", font=self.button_font, text="Name", height=1, width=9,fg="Black")
-        self.name_label.pack(side=LEFT,fill=BOTH,expand=True,padx=10,pady=10)
-        self.age_frame = Frame(self.content_frame, height=100, width=300)
-        self.age_frame.place(x=0, y=200)
-        self.age_label = Label(self.age_frame,bg="White", font=self.button_font, text="Age", height=1, width=9,fg="Black")
-        self.age_label.pack(side=BOTTOM,expand=True,padx=10,pady=10)
-        self.name_entry_frame = Frame(self.content_frame,height=100,width=100)
-        self.name_entry_frame.place(x=300,y=0)
-        self.name_entry = Entry(self.name_entry_frame,width=70)
-        self.name_entry.pack(side=RIGHT,ipady=10,fill=BOTH,expand=True)
-        self.age_entry_frame = Frame(self.content_frame, height=100, width=100)
-        self.age_entry_frame.place(x=300, y=200)
-        self.age_entry = Entry(self.age_entry_frame, width=70)
-        self.age_entry.pack(side=RIGHT, ipady=10, fill=BOTH, expand=True)
-        self.Continue_button_frame = Frame(self.content_frame, height=100, width=100)
-        self.Continue_button_frame.place(x=350,y=300)
-        self.continue_button = Button(self.Continue_button_frame,text="Continue",activebackground="Yellow", bd=3,bg="black", fg="white",
-                                          command=self.continue_frame1,font=self.button_font,justify=CENTER, height=2, width=20)
-        self.continue_button.pack(side=TOP)
+        self.CreateMainFrame()
+        self.heading_frame= Frame(self.main_frame,height=100,width=1100,bg="#1a0d00")
+        self.heading_frame.place(x=20,y=20)
+        self.heading = Message(self.heading_frame,font=self.heading_font,bg="#1a0d00",fg="White",width=1100,justify=CENTER,text="Let us start with your details")
+        self.heading.place(x=10,y=10)
+        self.content_frame = Frame(self.main_frame, height=150, width=700, bg="#1a0d00")
+        self.content_frame.place(x=300, y=300)
+        self.name_label = Label(self.content_frame, bg="#1a0d00", font=self.text_font, text="Name", height=1, width=9,fg="White")
+        self.name_label.place(x=10,y=10)
+        self.name_entry = Entry(self.content_frame, width=70,bd=3)
+        self.name_entry.place(x=100,y=10)
+        self.age_label = Label(self.content_frame, bg="#1a0d00", font=self.text_font, text="Age", height=1, width=9,fg="White")
+        self.age_label.place(x=10,y=50)
+        self.age_entry = Entry(self.content_frame, width=70,bd=3)
+        self.age_entry.place(x=100,y=50)
+        self.ButtonFrame = Frame(self.main_frame, height=50, width=600,bg="#1a0d00")
+        self.ButtonFrame.place(x=350, y=600)
+        self.continue_button = Button(self.ButtonFrame, text="Continue", activebackground="saddle brown", bd=3,bg="black", fg="white",
+                                      command=self.PlayPage, font=self.button_font, justify=CENTER, height=1,width=15)
+        self.continue_button.place(x=0,y=0)
+        self.back_button = Button(self.ButtonFrame,text="Back",activebackground="saddle brown",bd=3,bg="black",fg="white",
+                                  command=self.first_page,font=self.button_font,justify=CENTER,height=1,width=15)
+        self.back_button.place(x=285,y=0)
 
-
-    def play_friend(self):
-        pass
-    def about(self):
-        pass
-
-    def continue_frame1(self):
+    def PlayPage(self):
         self.age=self.age_entry.get()
         self.name=self.name_entry.get()
         self.name=self.name.replace(" ", "")
@@ -110,11 +66,27 @@ class Riddle_Game(object):
         else:
             self.name_entry.delete(0,len(self.name)+2)
             self.age_entry.delete(0, len(self.age))
-            self.alert_frame= Frame(self.content_frame,height=100,width=600)
-            self.alert_frame.place(x=300,y=250)
             self.alert = "Please enter valid details"
-            self.alert_info =Label(self.alert_frame,bg="White", font=self.button_font, text=self.alert, height=1, width=25,fg="Black")
-            self.alert_info.pack(side=TOP)
+            self.alert_info =Label(self.content_frame,bg="#1a0d00", font=self.text_font, text=self.alert, height=1, width=50,fg="White")
+            self.alert_info.place(x=80,y=100)
+
+    def CreateMainFrame(self,page="Any"):
+        self.main_frame = Frame(self.root, height=800, width=1200)
+        self.main_frame.place(x=0, y=0)
+        self.BackgroundImage(self.main_frame,page)
+
+    def BackgroundImage(self,root,page="Any"):
+        if page=="First":
+            self.background_image = ImageTk.PhotoImage(file="images/riddle_front.jpg")
+        else:
+            self.background_image = ImageTk.PhotoImage(file="images/riddle_simple.jpg")
+        self.background_label = Label(root, image=self.background_image)
+        self.background_label.grid(row=0, column=0)
+
+    def play_friend(self):
+        pass
+    def about(self):
+        pass
 
 if __name__ == '__main__':
     Riddle_Game()
